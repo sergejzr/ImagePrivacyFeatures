18a43dec7060f4bfbe0afbc01f8cd706519c9c45
zerr <zerr@l3s.de>
2016-09-08 18:49:54 +0100

    fixed pom

--- FeatureExtraction/src/main/java/de/l3s/image/Picture.java
+++ FeatureExtraction/src/main/java/de/l3s/image/Picture.java
@@ -6,6 +6,7 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileNotFoundException;
@@ -21,7 +22,7 @@ import java.util.Set;
 
 import javax.imageio.ImageIO;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+
 
 import sz.de.l3s.features.FeatureExtractor;
 import sz.de.l3s.features.VisualFeature;
@@ -504,7 +505,7 @@ public abstract class Picture {
 	public void storePictureFromBytes(File tmppicfolder, byte[] data) {
 
 		try {
-			BufferedImage img = ImageIO.read(new ByteInputStream(data, 0, data.length));
+			BufferedImage img = ImageIO.read(new ByteArrayInputStream(data, 0, data.length));
 			if (img.getWidth() > 500 || img.getHeight() > 500)
 				img = resizeImage(img, 500, 500);
 			ImageIO.write(img, "jpg", tmppicfolder);
